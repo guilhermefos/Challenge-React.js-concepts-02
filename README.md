e1a020dc6ec47d7b5ebb52374ec461773237033c
Guilherme Ferreira <guilhermefos.developer@gmail.com>
2020-06-19 17:12:41 -0300

    Implement the challange

---
+++ README.md
@@ -0,0 +1,34 @@
+# Challange React Js Concepts 02
+
+Challange React Js Concepts 02 is a project to training React Js basic concepts on [RocketSeat BootCamp 2020](https://rocketseat.com.br/) course.
+
+The objective of the project is to store, delete, and list repositories on Node.JS [backend](https://github.com/guilhermefos/RocketSeat-BootCamp-2020-node-basic-concepts)  project.
+
+### Features
+
+* List all repositories
+* Add a new repository
+* Remove a repository
+
+## Installation
+
+Use the package manager [yarn](https://yarnpkg.com/) to install Challange React Js Concepts 02.
+
+```bash
+yarn install
+```
+
+## Usage
+
+Run the project with command
+
+```bash
+yarn start
+```
+Run unit test's with command
+```bash
+yarn test
+```
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- src/App.js
+++ src/App.js
@@ -1,29 +1,90 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from './services/api'
 
 import "./styles.css";
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
+  const [repositories, setRepositories] = useState([])
+  const [repository, setRepository] = useState({})
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data)
+    })
+  }, [])
+
+  function handleInputChange(e) {
+    const { name, value } = e.target
+
+    if (name === 'techs') {
+      const techs = value.split(',').map(item => item.trim())
+      setRepository({ ...repository, techs })
+      return
+    }
+
+    setRepository({ ...repository, [name]: value })
+  }
+
+  async function handleAddRepository(e) {
+    e.preventDefault()
+    api.post('repositories', repository).then(response => {
+      setRepositories([...repositories, response.data])
+    })
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    api.delete(`repositories/${id}`).then(() => {
+      setRepositories(repositories.filter(repository => repository.id !== id))
+    })
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+      {repositories.map(repository => (
+        <li key={`${repository.id}`}>
+          {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
+          <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
           </button>
         </li>
+      ))}
       </ul>
 
-      <button onClick={handleAddRepository}>Adicionar</button>
+      <form onSubmit={handleAddRepository}>
+        <div>
+          <label htmlFor="url">Url</label>
+          <input
+            type="text"
+            name="url"
+            id="url"
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            name="title"
+            id="title"
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="techs">Techs</label>
+          <input
+            type="text"
+            name="techs"
+            id="techs"
+            onChange={handleInputChange}
+          />
+          <span>Adicione mais de um separando por virgula</span>
+        </div>
+        <button type="submit">Adicionar</button>
+      </form>
+
     </div>
   );
 }
